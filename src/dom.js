db22c882024195a9eeeda1c1d4e5f263f46ab23f
Satyam Yadav <satyamyadav3@gmail.com>
2019-02-03 13:26:35 +0530

    removes browser check from dom api

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,16 @@
 import domApi from './domApi';
 
-export default domApi();
+export default domApi(['div',
+  'form',
+  'input',
+  'select',
+  'label',
+  'br',
+  'button',
+  'option',
+  'datalist',
+  'legend',
+  'fieldset',
+  'optgroup',
+  'progress',
+  'textarea']);
